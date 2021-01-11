276a14b616df8096dc13cb6b679f35391c5704b2
Daniel Mendoza <dan.ms.0739@gmail.com>
2021-01-11 17:01:03 -0600

    Added Push Kit

---
+++ app/src/main/java/com/hms/demo/hmscoursedemoa/MyHmsMessageService.java
@@ -0,0 +1,20 @@
+package com.hms.demo.hmscoursedemoa;
+
+import android.util.Log;
+
+import com.huawei.hms.push.HmsMessageService;
+import com.huawei.hms.push.RemoteMessage;
+
+public class MyHmsMessageService extends HmsMessageService {
+
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.e("Token",token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
