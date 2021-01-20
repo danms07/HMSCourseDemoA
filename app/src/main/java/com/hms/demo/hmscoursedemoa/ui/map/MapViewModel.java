7ae81443f5b120420033cbce3b5e2615fa3dbd01
Daniel Mendoza <dan.ms.0739@gmail.com>
2021-01-19 19:09:24 -0600

    Added siteKit

--- app/src/main/java/com/hms/demo/hmscoursedemoa/ui/map/MapViewModel.java
+++ app/src/main/java/com/hms/demo/hmscoursedemoa/ui/map/MapViewModel.java
@@ -3,18 +3,32 @@ package com.hms.demo.hmscoursedemoa.ui.map;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import com.huawei.agconnect.config.AGConnectServicesConfig;
 import com.huawei.hms.common.ResolvableApiException;
 import com.huawei.hms.maps.CameraUpdate;
 import com.huawei.hms.maps.CameraUpdateFactory;
 import com.huawei.hms.maps.HuaweiMap;
 import com.huawei.hms.maps.OnMapReadyCallback;
 import com.huawei.hms.maps.model.LatLng;
+import com.huawei.hms.maps.model.PointOfInterest;
+import com.huawei.hms.site.api.SearchResultListener;
+import com.huawei.hms.site.api.SearchService;
+import com.huawei.hms.site.api.SearchServiceFactory;
+import com.huawei.hms.site.api.model.DetailSearchRequest;
+import com.huawei.hms.site.api.model.DetailSearchResponse;
+import com.huawei.hms.site.api.model.SearchStatus;
+import com.huawei.hms.site.api.model.Site;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class MapViewModel extends ViewModel
-        implements OnMapReadyCallback,GPS.OnGPSEventListener {
+        implements OnMapReadyCallback, GPS.OnGPSEventListener,
+        HuaweiMap.OnPoiClickListener, SearchResultListener<DetailSearchResponse> {
 
     private MapNavigator navigator;
     private boolean locationPermissions;
@@ -30,6 +44,7 @@ public class MapViewModel extends ViewModel
 
     @Override
     public void onMapReady(HuaweiMap huaweiMap) {
+        huaweiMap.setOnPoiClickListener(this);
         this.map=huaweiMap;
     }
 
@@ -93,8 +108,39 @@ public class MapViewModel extends ViewModel
         }
     }
 
+    @Override
+    public void onPoiClick(PointOfInterest pointOfInterest) {
+        if(navigator!=null){
+            SearchService service=navigator.loadSearchService();
+            if(service!=null){
+                DetailSearchRequest request=new DetailSearchRequest();
+                request.siteId=pointOfInterest.placeId;
+                service.detailSearch(request, this);
+            }
+        }
+    }
+
+    @Override
+    public void onSearchResult(DetailSearchResponse detailSearchResponse) {
+        if(detailSearchResponse!=null){
+            if(detailSearchResponse.site!=null&&navigator!=null){
+                navigator.displaySiteDialog(detailSearchResponse.site);
+            }
+        }
+    }
+
+    @Override
+    public void onSearchError(SearchStatus searchStatus) {
+        Log.e(
+                "MapViewModel",
+                "Error : " + searchStatus.errorCode + " " + searchStatus.errorMessage
+        );
+    }
+
     interface MapNavigator{
         void requestLocationPermissions();
         void startResolutionForResult(ResolvableApiException e);
+        SearchService loadSearchService();
+        void displaySiteDialog(Site site);
     }
 }
